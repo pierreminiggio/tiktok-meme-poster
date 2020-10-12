9778d3a7f092ae40e1a0de1c9ca56ebe894688d4
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-12 13:11:11 +0200

    intervention resize

--- server/src/ImageUploader.php
+++ server/src/ImageUploader.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Intervention\Image\ImageManager;
+
 class ImageUploader
 {
     public function upload(?array $image)
@@ -30,8 +32,23 @@ class ImageUploader
         $memeImagePath = __DIR__ . '/../public/images/' . $i . '_sized.png';
         if (! file_exists($originalImagePath)) {
             copy($uploadedImage, $originalImagePath);
+            $manager = new ImageManager(['driver' => 'imagick']);
+            $image = $manager->make($originalImagePath);
 
-            return $originalImagePath;
+            $originalWidth = $image->width();
+            $originalHeight = $image->height();
+            if ($originalWidth > $originalHeight) {
+                $width = 1080;
+                $height = (int) ((1080 / $originalWidth) * $originalHeight);
+            } else {
+                $height = 1920;
+                $width = (int) ((1920 / $originalHeight) * $originalWidth);
+            }
+            $image->resize($width, $height);
+            $image->resizeCanvas(1080, 1920);
+            $image->save($memeImagePath, 80, 'png');
+            
+            return $memeImagePath;
         }
 
         return $this->generateNewImage($uploadedImage, $i + 1);
