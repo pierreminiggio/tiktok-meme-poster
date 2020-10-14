f88be3e541e2a3d79c0e21f0057bd863c2c16031
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-14 20:06:19 +0200

    post 1st entry from queue

--- queuePoster.js
+++ queuePoster.js
@@ -1,1 +1,49 @@
-console.log('test')
+const ids = require('./ids.json')
+const post = require('@pierreminiggio/tiktok-poster')
+const fs = require('fs')
+
+/**
+ * @typedef {Object} TikTok
+ * 
+ * @property {string} video
+ * @property {string} legend
+ */
+
+ /**
+  * @returns {Promise}
+  */
+function postFirstTikTokInQueue() {
+
+    return new Promise(async (resolve, rejects) => {
+
+        const queueFile = './queue.json'
+        const postedFile = './posted.json'
+
+        /** @type {TikTok[]} queue */
+        const queue = require(queueFile)
+
+        /** @type {TikTok[]} posted */
+        const posted = require(postedFile)
+
+        if (queue.length) {
+            const key = 0
+            const toPost = queue[key]
+            await post(ids.login, ids.password, toPost.video, toPost.legend)
+            queue.splice(key, 1)
+            posted.push(toPost)
+            fs.writeFile(queueFile, JSON.stringify(queue), (err) => {
+                if (err) {
+                    rejects()
+                }
+                fs.writeFile(postedFile, JSON.stringify(posted), (err) => {
+                    if (err) {
+                        rejects()
+                    }
+                    resolve()
+                })
+            })
+        }
+    })
+}
+
+postFirstTikTokInQueue()
