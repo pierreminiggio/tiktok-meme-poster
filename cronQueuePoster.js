853b4698a6495f89046a8fb4c754a4eabbd7fcf4
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-15 18:34:56 +0200

    show some logs

--- cronQueuePoster.js
+++ cronQueuePoster.js
@@ -1,6 +1,9 @@
 const postFirstTikTokInQueue = require('./queuePoster.js')
 const cron = require('node-cron')
 
-cron.schedule('0 * * * *', () => {
-    postFirstTikTokInQueue()
+cron.schedule('0 * * * *', async () => {
+    console.log(new Date().toLocaleTimeString())
+    console.log('Try posting...')
+    await postFirstTikTokInQueue()
+    console.log('Posted !')
 })
